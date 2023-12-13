265a4950aaacec584c2b5b719414fbac53880344
wafiben <wafibenjeddou@gmail.com>
2023-12-13 04:18:01 +0100

    first

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
    const path='/home/benjeddou/Desktop/nest-grpc/src/proto/service.proto'
+   /* ll */
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
